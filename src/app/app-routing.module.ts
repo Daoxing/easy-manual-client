c67cfef1b8efc0c7d0a9293a29ce93b88bf752d5
Daoxing <li.dao@husky.neu.edu>
2021-02-23 14:47:07 +0800

    easy manual basic setting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ArticleComponent } from './components/article/article.component';
+import { HomeComponent } from './components/home/home.component';
+import { EditingArticleComponent } from './components/editing-article/editing-article.component';
+const routes: Routes = [
+  {
+    path: 'home',
+    redirectTo: '/home',
+  },
 
-const routes: Routes = [];
+  { path: 'home', component: HomeComponent },
+  { path: 'article', component: ArticleComponent },
+  { path: 'article/edit', component: EditingArticleComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  declarations: [],
+  imports: [RouterModule.forRoot(routes), CommonModule],
 })
 export class AppRoutingModule {}
